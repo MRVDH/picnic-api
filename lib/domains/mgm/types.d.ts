e581e06ff6c960694ba69c8925195703f2a189c2
MRVDH <info@maartenvandenhoven.com>
2026-07-02 22:48:07 +0200

    chore: cleanup stale lib artifacts and bump version to 4.6.0

--- lib/domains/mgm/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type MgmDetails = {
-    mgm_code: string;
-    invitee_value: number;
-    inviter_value: number;
-    share_url: string;
-    amount_earned: number;
-};
-//# sourceMappingURL=types.d.ts.map
